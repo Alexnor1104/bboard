8910e0dc67d2adc9fdaf395c6f747fc76a9f038e
Alexnor11 <advance114@gmail.com>
2022-01-26 13:31:40 +0300

    Basic model

--- main/models.py
+++ main/models.py
@@ -10,3 +10,11 @@ class AdvUser(AbstractUser):
 
     class Meta(AbstractUser.Meta):
         pass
+
+
+# Базовая модель рубрик
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, unique=True, verbose_name='Название')
+    order = models.SmallIntegerField(default=0, db_index=True, verbose_name='Порядок')
+    super_rubric = models.ForeignKey('SuperRubric', on_delete=models.PROTECT,
+                                     null=True, blank=True, verbose_name='Надрубрика')
